cafe42d3c8d880d40202a40c0f5907230c64b79b
Aaron Honeycutt <aaronhoneycutt@ubuntu.com>
2016-09-03 21:16:07 -0400

    Update Vivaldi version, adding snap support

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # fresh-install-script
 A script to help me update, upgrade and install my most used software in a quick and clean manner.
 
+A lot of the bash credit goes to Marius Quabeck for his awesome magic-device-tool: https://github.com/MariusQuabeck/magic-device-tool
+
 Notes
 -------
 The Chrome install option is a WIP and selecting it will move to the next step of the script.
--- fresh-install.sh
+++ fresh-install.sh
@@ -8,6 +8,7 @@ echo "Let's make sure your sources are up to date"
 echo "running apt update"
 echo "-------------------------------"
 sleep 3
+clear
 sudo apt update
 sleep 1
 echo "Clearing the screen"
@@ -20,6 +21,15 @@ echo "-------------------------------"
 sudo -k apt upgrade
 sleep 1
 clear
+	if [ $(dpkg-query -W -f='${Status}' mplayer 2>/dev/null | grep -c "ok installed") -eq 0 ];
+	then
+	  echo ""
+	  echo "First we have to install the necessary tools:"
+	  echo ""
+	  echo "  → snapd"
+	  echo ""
+	  sudo apt-get install -qq -y snapd;
+	fi
 echo "-------------------------------"
 echo "Install your browser of choice"
 echo ""
@@ -32,7 +42,7 @@ echo -n "Enter choice: "; read browserchoice
 if [ "$browserchoice" = "2" ]; then
 sudo -k apt install chromium-browser
 elif [ "$browserchoice" = "3" ]; then
-wget https://downloads.vivaldi.com/stable/vivaldi-stable_1.3.551.30-1_amd64.deb 
+wget https://downloads.vivaldi.com/stable/vivaldi-stable_1.3.551.38-1_amd64.deb 
 sudo dpkg -i vivaldi-stable_1.3.551.30-1_amd64.deb
 fi
 sleep 1
@@ -42,7 +52,8 @@ echo "Install your graphics software of choice"
 echo ""
 echo "[1] GIMP"
 echo "[2] Inkscape"
-echo "[3] Install all"
+echo "[3] Inkscape snap"
+echo "[4] Install all"
 echo "-------------------------------"
 echo ""
 echo -n "Enter choice: "; read graphicschoice
@@ -51,6 +62,9 @@ sudo apt install gimp
 elif [ "$graphicschoice" = "2" ]; then
 sudo apt install inkscape
 elif [ "$graphicschoice" = "3" ]; then
+wget https://launchpad.net/~inkscape-uploader/+snap/inkscape/+build/4308/+files/inkscape_0.91+devel_amd64.snap
+sudo snap install inkscape_0.91+devel_amd64.snap
+elif [ "$graphicschoice" = "4" ]; then
 sudo apt install gimp inkscape
 fi
 sleep 1
