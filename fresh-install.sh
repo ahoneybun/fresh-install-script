795f8758cd44e624a49f237e276ba897656a0c02
Aaron Honeycutt <aaronhoneycutt@kubuntu.org>
2016-10-27 19:20:18 -0400

    removed git, added official stable kdenlive PPA

--- fresh-install.sh
+++ fresh-install.sh
@@ -105,6 +105,10 @@ echo ""
 echo -n "Enter choice: "; read avchoice
     if [ "$avchoice" = "1" ]; then
      sudo apt install kdenlive
+      elif [ "$avchoice" = "1a" ]; then
+       sudo add-apt-repository ppa:kdenlive/kdenlive-stable
+       sudo apt update
+       sudo apt install kdenlive
       elif [ "$avchoice" = "1b" ]; then
        sudo snap install --edge --force-dangerous --devmode kdenlive-devel
     fi
@@ -144,7 +148,7 @@ echo "-------------------------------"
 echo "Installing some tools"
 echo ""
 echo "pastebinit, git, bzr, virtualbox, rememberthemilk, htop"
-sudo apt install pastebinit git bzr htop
+sudo apt install pastebinit bzr htop
 
 # Downloading virtualbox and rememberthemilk
   wget http://download.virtualbox.org/virtualbox/5.1.6/virtualbox-5.1_5.1.6-110634~Ubuntu~xenial_amd64.deb
