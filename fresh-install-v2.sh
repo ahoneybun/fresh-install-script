db0e4202c87426ab0e6c1e9bbfa72cb7f92886bd
Aaron Honeycutt <aaronhoneycutt@kubuntu.org>
2019-01-04 19:55:50 -0700

    clean up the mess

--- fresh-install-v2.sh
+++ fresh-install-v2.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # Intro
 # -------------
-echo "-------------------------------"
+echo "------------------------------------------------------------------"
 distro="$(cat /etc/os-release | awk -F "\"" '/PRETTY_NAME/ {print $2}')"
 echo "Welcome to your fresh" "${distro}"
-echo "-------------------------------"
+echo "------------------------------------------------------------------"
 
 # Updating
 # -------------
 echo ""
+echo "--------------------------------------------"
 echo "Let's make sure your sources are up to date"
 echo "running apt update"
-echo "-------------------------------"
+echo "--------------------------------------------"
 sleep 3
 sudo apt update
 sleep 1
@@ -75,45 +76,14 @@ esac
 sleep 1
 clear
 
-# Video and Audio selection
-# --------------------------
-echo "-------------------------------"
-echo "Install your AV tools of choice"
-echo ""
-echo "[1] kdenlive"
-echo "[1a] kdenlive PPA"
-echo "[2] Audacity"
-echo "[3] kdenlive & Audacity"
-echo "[4] VLC"
-echo "[0] Next"
-echo "-------------------------------"
-echo ""
-echo -n "Enter choice: "; read avchoice
-    if [ "$avchoice" = "1" ]; then
-     sudo apt install kdenlive
-      elif [ "$avchoice" = "1a" ]; then
-       sudo add-apt-repository ppa:kdenlive/kdenlive-stable
-       sudo apt update
-       sudo apt install kdenlive
-      elif [ "$avchoice" = "2" ]; then
-       sudo apt install audacity
-      elif [ "$avchoice" = "3" ]; then
-       sudo apt install kdenlive audacity
-      elif [ "$avchoice" = "4" ]; then
-       sudo apt install vlc
-     elif [ "$avchoice" = "0"];then
-       exit
-    fi
-sleep 1
-clear
-
 # Random useful tools
 # -------------------
 echo "-------------------------------"
 echo "Installing some tools"
 echo ""
 echo "fish"
-
+echo "-------------------------------"
+echo ""
 wget https://github.com/fish-shell/fish-shell/releases/download/3.0.0/fish-3.0.0.tar.gz
 tar -xvf fish-3.0.0.tar.gz
 cd fish-3.0.0/
@@ -124,10 +94,15 @@ chsh -s /usr/local/bin/fish
 # Fixing dep issues
 # and removing old packages
 # --------------------------
+echo "-------------------------------------"
+echo "Now let's clean up some dependencies"
+echo "-------------------------------------"
+echo ""
 sudo apt -f install
 sudo apt autoremove
+rm -r fish-3.0.0 && rm -r fish-3.0.0.tar.gz
 clear
-echo "-------------------------------"
+echo "---------------------------------"
 echo "Have a productive and happy day!"
 echo "End of script"
-echo "-------------------------------"
+echo "---------------------------------"
