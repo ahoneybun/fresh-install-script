6d68bada730e4bb32aca205c9a0293eae9f0e71e
Aaron Honeycutt <aaron@system76.com>
2021-07-17 14:26:21 -0600

    cleaning up the debugging echo commands

--- main.sh
+++ main.sh
@@ -6,35 +6,35 @@ zenity --info --title="Fresh Install Tool" --text="Let's walk though what softwa
 ## Fedora
 
 if [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Fedora"* ]]; then
-   echo "btw I use Fedora"
+   echo "Fedora detected"
 
    lib/fedora.sh
 
 ## Ubuntu
 
 elif [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Ubuntu"* ]]; then
-   echo "btw I use Ubuntu"
+   echo "Ubuntu detected"
 
    lib/ubuntu.sh
 
 ## Pop!_OS
 
 elif [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Pop"* ]]; then
-   echo "btw I use Pop!_OS"
+   echo "Pop!_OS detected"
 
    lib/pop.sh
 
 ## Arch
 
    elif [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Arch Linux"* ]]; then
-      echo "btw I use Arch"
+      echo "Arch detected"
 
    lib/arch.sh
 
 ## Garuda
 
    elif [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Garuda Linux"* ]]; then
-      echo "btw I use Garuda (cause I can't install Arch)"
+      echo "Garuda detected"
 
    lib/garuda.sh
 
